c2c2217d3dfee258796e80ac7956d1060b80c3f4
Olivia Park <olivia.park1346@gmail.com>
2020-07-31 19:42:28 +0900

    error update

--- app.js
+++ app.js
@@ -1,21 +1,50 @@
 const express = require('express');
+const {projects} = require('./data.json');
+const port = process.env.PORT || 3000;
+const app = express();
+
+//middleware
+app.set('view engine', 'pug');
+app.use(express.json());
+//static route
 app.use('/static', express.static('public'));
 
-app.set('view engine', 'pug');
-
 //index
-app.get('/', (req, res, next) => {
-  res.render('index');
-})
+app.get('/',(req, res, next) => {
+  res.render('index', {projects});
+});
+
 //about
-app.get('/about', (req, res, next) => {
+app.get('/about',(req, res, next) => {
   res.render('about');
-})
-//dynamic project
+});
+
+//projects
 app.get('/projects/:id',(req, res, next) => {
+  const projectId = req.params.id;
+  const project = projects.find(({id}) => id === +projectId);
 
-})
+  if (project) {
+  res.render('project', {project});
+  } else {
+    res.sendStatus(404);
+  }
+});
 
-app.listen(3000, () => {
-  console.log('The application is running on localhost:3000!')
+//error handling
+app.use((req, res, next) => {
+  const err = new Error('Page not found!');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
+});
+
+//server
+app.listen(port, () => {
+  console.log('The application is running on localhost:3000!');
 });
